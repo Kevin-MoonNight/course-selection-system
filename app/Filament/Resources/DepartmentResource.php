f791caf138419bb5f647a2947ef1f87d6da16d8f
KevinMoonNight <k0937477331@gmail.com>
2022-12-08 21:38:50 +0800

    feat:Add department crud pages

---
+++ app/Filament/Resources/DepartmentResource.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\DepartmentResource\Pages;
+use App\Filament\Resources\DepartmentResource\RelationManagers;
+use App\Models\Department;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class DepartmentResource extends Resource
+{
+    protected static ?string $model = Department::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('id')
+                    ->label('系碼')
+                    ->required()
+                    ->maxLength(4),
+                Forms\Components\TextInput::make('name')
+                    ->label('系名')
+                    ->required()
+                    ->maxLength(10),
+                Forms\Components\TextInput::make('name')
+                    ->label('系主任')
+                    ->required()
+                    ->maxLength(4),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')
+                    ->label('系碼'),
+                Tables\Columns\TextColumn::make('name')
+                    ->label('系名'),
+                Tables\Columns\TextColumn::make('chair')
+                    ->label('系主任'),
+            ])
+            ->filters([
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make()
+                    ->label('修改'),
+                Tables\Actions\DeleteAction::make()
+                    ->label('刪除'),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make()
+                    ->label('刪除所有'),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDepartments::route('/'),
+            'create' => Pages\CreateDepartment::route('/create'),
+            'edit' => Pages\EditDepartment::route('/{record}/edit'),
+        ];
+    }
+}
